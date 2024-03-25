05651200d5e61df53945a475436abe2b25902b26
ArtisDairis <artisdairis@gmail.com>
2024-03-25 21:50:26 +0200

    25.03 update

--- scripts/animators_section.js
+++ scripts/animators_section.js
@@ -19,21 +19,26 @@ for (i = 0; i < acc.length; i++)
     });
 }
 
-function openCharInfo(this_elem, elem) {
+function openCharInfo(this_elem, elem) 
+{
   let element = document.getElementById("c" + elem);
   let chevron = document.getElementById("ch_down_" + elem);
   let tabcontent = document.getElementsByClassName("tabcontent");
-  for (let j = 0; j < tabcontent.length; j++) {
+  for (let j = 0; j < tabcontent.length; j++) 
+  {
       tabcontent[j].hidden = true;
       document.getElementById("ch_down_" + (j+1)).classList.replace("fa-chevron-up", "fa-chevron-down");
 
   }
 
-  if (this_elem.classList.contains("none")) {
+  if (this_elem.classList.contains("none")) 
+  {
       this_elem.classList.replace("none", "show");
       chevron.classList.replace("fa-chevron-down", "fa-chevron-up");
       element.hidden = false;
-  } else {
+  } 
+  else 
+  {
       this_elem.classList.replace("show", "none");
       chevron.classList.replace("fa-chevron-up", "fa-chevron-down");
       element.hidden = true;
